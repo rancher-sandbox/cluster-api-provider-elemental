419c84b4f9d6b063b30e6ac2c4e8610ae730ec32
Andrea Mazzotti <andrea.mazzotti@suse.com>
2024-08-28 08:55:31 +0200

    Use same OS image for kubeadm (#81)
    Signed-off-by: Andrea Mazzotti <andrea.mazzotti@suse.com>

--- Dockerfile.os
+++ Dockerfile.os
@@ -1,55 +1,14 @@
 ARG ELEMENTAL_TOOLKIT=ghcr.io/rancher/elemental-toolkit/elemental-cli:nightly
+ARG ELEMENTAL_AGENT=ghcr.io/rancher-sandbox/cluster-api-provider-elemental/agent:latest
 
-FROM registry.opensuse.org/opensuse/leap:15.5 as AGENT
-
-# Install Go 1.22
-RUN zypper install -y wget tar gzip gcc
-RUN wget https://go.dev/dl/go1.22.0.linux-amd64.tar.gz
-RUN tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz
-ENV PATH "$PATH:/usr/local/go/bin"
-
-# Copy the Go Modules manifests
-WORKDIR /workspace
-COPY go.mod go.mod
-COPY go.sum go.sum
-# cache deps before building and copying source so that we don't need to re-download as much
-# and so that source changes don't invalidate our downloaded layer
-RUN go mod download
-
-# Copy the go source
-COPY cmd/agent/main.go cmd/agent/main.go
-COPY api/ api/
-COPY internal/ internal/
-COPY pkg/ pkg/
-
-ARG TAG=v0.0.0
-ARG COMMIT=""
-ARG COMMITDATE=""
-
-# Build agent binary
-RUN CGO_ENABLED=1 go build \
-    -ldflags "-w -s  \
-    -X github.com/rancher-sandbox/cluster-api-provider-elemental/internal/version.Version=$TAG  \
-    -X github.com/rancher-sandbox/cluster-api-provider-elemental/internal/version.Commit=$COMMIT  \
-    -X github.com/rancher-sandbox/cluster-api-provider-elemental/internal/version.CommitDate=$COMMITDATE"  \
-    -a -o elemental-agent cmd/agent/main.go
-
-# Build elemental-toolkit plugin
-RUN CGO_ENABLED=1 go build \
-    -buildmode=plugin \
-    -o elemental.so internal/agent/plugin/elemental/elemental.go
-
-# Build dummy plugin
-RUN CGO_ENABLED=1 go build \
-    -buildmode=plugin \
-    -o dummy.so internal/agent/plugin/dummy/dummy.go
-
+FROM ${ELEMENTAL_AGENT} as AGENT
 FROM  ${ELEMENTAL_TOOLKIT} as TOOLKIT
 
 # OS base image of our choice
-FROM registry.opensuse.org/opensuse/leap:15.5 as OS
+FROM registry.opensuse.org/opensuse/tumbleweed:latest as OS
 
 ARG AGENT_CONFIG_FILE=iso/config/example-config.yaml
+ARG KUBEADM_READY
 
 # install kernel, systemd, dracut, grub2 and other required tools
 RUN ARCH=$(uname -m); \
@@ -94,9 +53,26 @@ RUN ARCH=$(uname -m); \
 # Add the elemental cli
 COPY --from=TOOLKIT /usr/bin/elemental /usr/bin/elemental
 # Add the elemental-agent and plugins
-COPY --from=AGENT /workspace/elemental-agent /usr/sbin/elemental-agent
-COPY --from=AGENT /workspace/elemental.so /usr/lib/elemental/plugins/elemental.so
-COPY --from=AGENT /workspace/dummy.so /usr/lib/elemental/plugins/dummy.so
+COPY --from=AGENT /elemental-agent /usr/sbin/elemental-agent
+COPY --from=AGENT /elemental.so /usr/lib/elemental/plugins/elemental.so
+COPY --from=AGENT /dummy.so /usr/lib/elemental/plugins/dummy.so
+
+# Install kubeadm stack dependencies
+RUN if [ -n "${KUBEADM_READY}" ]; then \
+    zypper --non-interactive install -- \
+    conntrackd \
+    conntrack-tools \
+    iptables \
+    ebtables \
+    buildah \
+    ethtool \
+    socat; \
+    fi;
+
+# Install kubeadm stack
+COPY test/scripts/install_kubeadm_stack.sh /tmp/install_kubeadm_stack.sh
+RUN if [ -n "${KUBEADM_READY}" ]; then /tmp/install_kubeadm_stack.sh; fi;
+RUN rm -f /tmp/install_kubeadm_stack.sh
 
 # Add framework files
 COPY framework/files/ /
@@ -106,6 +82,14 @@ COPY $AGENT_CONFIG_FILE /oem/elemental/agent/config.yaml
 
 # Enable essential services
 RUN systemctl enable NetworkManager.service sshd
+
+# Enable kubeadm needed services
+RUN if [ -n "${KUBEADM_READY}" ]; then \
+    systemctl enable conntrackd containerd kubelet; \
+    fi;
+
+# !!! This is for testing purposes, do not do this in production. !!!
+RUN echo "PermitRootLogin yes" > /etc/ssh/sshd_config.d/rootlogin.conf
 
 # Make sure trusted certificates are properly generated
 RUN /usr/sbin/update-ca-certificates
@@ -121,8 +105,10 @@ RUN zypper clean --all && \
     >/var/log/lastlog && \
     rm -rf /boot/vmlinux*
 
-# Enable /tmp to be on tmpfs
-RUN cp /usr/share/systemd/tmp.mount /etc/systemd/system
+# Ensure /tmp is mounted as tmpfs by default
+RUN if [ -e /usr/share/systemd/tmp.mount ]; then \
+      cp /usr/share/systemd/tmp.mount /etc/systemd/system; \
+    fi
 
 # Required by k3s/rke2
 RUN mkdir -p /usr/libexec && touch /usr/libexec/.keep
--- test/scripts/install_kubeadm_stack.sh
+++ test/scripts/install_kubeadm_stack.sh
@@ -3,40 +3,45 @@
 # See: https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/#installing-kubeadm-kubelet-and-kubectl
 # See: https://github.com/go4clouds/cloud-infra/blob/main/libvirt/provision-k8s-node.sh
 
-# Mind the /usr/local/sbin hack in ../../framework/files/system/oem/99_kubeadm_hack.yaml
-# Additional binaries to pass preflight (if any) need to be in PATH
-
 set -e
 
 ARCH=$(uname -m)
 if [[ $ARCH == "aarch64" ]]; then ARCH="arm64"; fi;
 if [[ $ARCH == "x86_64" ]]; then ARCH="amd64"; fi;
 
-KUBEADM_DIR="/opt/kubeadm/bin"
+KUBEADM_DIR="/usr/bin"
 mkdir -p "$KUBEADM_DIR"
 
 ## CNI Plugins
-CNI_PLUGINS_VERSION="v1.4.1"
+## See: https://github.com/containernetworking/plugins
+echo "Installing CNI Plugins"
+CNI_PLUGINS_VERSION=${CNI_PLUGINS_VERSION:="v1.5.1"}
 DEST="/opt/cni/bin"
 mkdir -p "$DEST"
 curl -L "https://github.com/containernetworking/plugins/releases/download/${CNI_PLUGINS_VERSION}/cni-plugins-linux-${ARCH}-${CNI_PLUGINS_VERSION}.tgz" | tar -C "$DEST" -xz
 
 ## crictl
-CRICTL_VERSION="v1.29.0"
-curl -L "https://github.com/kubernetes-sigs/cri-tools/releases/download/${CRICTL_VERSION}/crictl-${CRICTL_VERSION}-linux-${ARCH}.tar.gz" | tar -C $KUBEADM_DIR -xz
+## See: https://github.com/kubernetes-sigs/cri-tools
+##      https://github.com/kubernetes-sigs/cri-tools?tab=readme-ov-file#compatibility-matrix-cri-tools--kubernetes
+echo "Installing crictl"
+CRICTL_VERSION=${CRICTL_VERSION:="v1.31.1"}
+curl -L "https://github.com/kubernetes-sigs/cri-tools/releases/download/${CRICTL_VERSION}/crictl-${CRICTL_VERSION}-linux-${ARCH}.tar.gz" | tar -C "$KUBEADM_DIR" -xz
 
 ## kubeadm/kubelet
 ## See: https://www.downloadkubernetes.com/
-RELEASE="v1.29.3"
+echo "Installing kubeadm/kubelet"
+K8S_RELEASE=${K8S_RELEASE:="v1.31.0"}
 ARCH="amd64"
 cd $KUBEADM_DIR
-curl -L --remote-name-all https://dl.k8s.io/release/${RELEASE}/bin/linux/${ARCH}/{kubeadm,kubelet}
+curl -L --remote-name-all https://dl.k8s.io/release/${K8S_RELEASE}/bin/linux/${ARCH}/{kubeadm,kubelet}
 chmod +x {kubeadm,kubelet}
 
-RELEASE_VERSION="v0.16.5"
+## kubeadm/kubelet configs
+## See: https://github.com/kubernetes/release/tree/master
+K8S_RELEASE_VERSION=${K8S_RELEASE_VERSION:="v0.17.2"}
 mkdir -p /usr/lib/systemd/system/kubelet.service.d
 
-#curl -sSL "https://raw.githubusercontent.com/kubernetes/release/${RELEASE_VERSION}/cmd/krel/templates/latest/kubelet/kubelet.service" | sed "s:/usr/bin:${KUBEADM_DIR}:g" | tee /usr/lib/systemd/system/kubelet.service
+#curl -sSL "https://raw.githubusercontent.com/kubernetes/release/${K8S_RELEASE_VERSION}/cmd/krel/templates/latest/kubelet/kubelet.service" -o /usr/lib/systemd/system/kubelet.service
 cat >> /usr/lib/systemd/system/kubelet.service << EOF
 [Unit]
 Description=kubelet: The Kubernetes Node Agent
@@ -57,20 +62,22 @@ RestartSec=10
 WantedBy=multi-user.target
 EOF
 
-curl -sSL "https://raw.githubusercontent.com/kubernetes/release/${RELEASE_VERSION}/cmd/krel/templates/latest/kubeadm/10-kubeadm.conf" | sed "s:/usr/bin:${KUBEADM_DIR}:g" | tee /usr/lib/systemd/system/kubelet.service.d/10-kubeadm.conf
+curl -sSL "https://raw.githubusercontent.com/kubernetes/release/${K8S_RELEASE_VERSION}/cmd/krel/templates/latest/kubeadm/10-kubeadm.conf" -o /usr/lib/systemd/system/kubelet.service.d/10-kubeadm.conf
 
 ## kubectl
-curl -LO https://dl.k8s.io/release/${RELEASE}/bin/linux/amd64/kubectl
+echo "Installing kubectl"
+curl -LO https://dl.k8s.io/release/${K8S_RELEASE}/bin/linux/amd64/kubectl
 chmod +x kubectl
 
 ## containerd
-CONTAINERD_VERSION="1.7.14"
-CONTAINERD_DIR="/opt/containerd/bin"
+## See: https://github.com/containerd/containerd
+echo "Installing containerd"
+CONTAINERD_VERSION=${CONTAINERD_VERSION:="1.7.20"}
+CONTAINERD_DIR="/usr/local/bin"
 mkdir -p "$CONTAINERD_DIR"
 cd $CONTAINERD_DIR
 curl -L "https://github.com/containerd/containerd/releases/download/v${CONTAINERD_VERSION}/containerd-${CONTAINERD_VERSION}-linux-${ARCH}.tar.gz" | tar  --strip-components=1 -C "$CONTAINERD_DIR" -xz
-
-#curl -sSL "https://raw.githubusercontent.com/containerd/containerd/main/containerd.service" | sed "s:/usr/bin:${CONTAINERD_DIR}:g" | tee /usr/lib/systemd/system/containerd.service
+#curl -sSL "https://raw.githubusercontent.com/containerd/containerd/v${CONTAINERD_VERSION}/containerd.service" -o /usr/lib/systemd/system/containerd.service
 cat >> /usr/lib/systemd/system/containerd.service << EOF
 # Copyright The containerd Authors.
 #
@@ -95,6 +102,8 @@ ConditionPathExists=!/run/elemental/live_mode
 ConditionPathExists=!/run/elemental/recovery_mode
 
 [Service]
+#uncomment to enable the experimental sbservice (sandboxed) version of containerd/cri integration
+#Environment="ENABLE_CRI_SANDBOXES=sandboxed"
 ExecStartPre=-/sbin/modprobe overlay
 ExecStart=$CONTAINERD_DIR/containerd
 
@@ -103,12 +112,11 @@ Delegate=yes
 KillMode=process
 Restart=always
 RestartSec=5
-
 # Having non-zero Limit*s causes performance problems due to accounting overhead
 # in the kernel. We recommend using cgroups to do container-local accounting.
 LimitNPROC=infinity
 LimitCORE=infinity
-
+LimitNOFILE=infinity
 # Comment TasksMax if your systemd version does not supports it.
 # Only systemd 226 and above support this version.
 TasksMax=infinity
@@ -120,307 +128,11 @@ EOF
 
 ## containerd config
 ## Generate a default config with 'containerd config default'
-
 mkdir -p /etc/containerd
-cat >> /etc/containerd/config.toml << EOF
-disabled_plugins = []
-imports = []
-oom_score = 0
-plugin_dir = ""
-required_plugins = []
-root = "/var/lib/containerd"
-state = "/run/containerd"
-temp = ""
-version = 2
-
-[cgroup]
-  path = ""
-
-[debug]
-  address = ""
-  format = ""
-  gid = 0
-  level = "debug"
-  uid = 0
-
-[grpc]
-  address = "/run/containerd/containerd.sock"
-  gid = 0
-  max_recv_message_size = 16777216
-  max_send_message_size = 16777216
-  tcp_address = ""
-  tcp_tls_ca = ""
-  tcp_tls_cert = ""
-  tcp_tls_key = ""
-  uid = 0
-
-[metrics]
-  address = ""
-  grpc_histogram = false
-
-[plugins]
-
-  [plugins."io.containerd.gc.v1.scheduler"]
-    deletion_threshold = 0
-    mutation_threshold = 100
-    pause_threshold = 0.02
-    schedule_delay = "0s"
-    startup_delay = "100ms"
-
-  [plugins."io.containerd.grpc.v1.cri"]
-    cdi_spec_dirs = ["/etc/cdi", "/var/run/cdi"]
-    device_ownership_from_security_context = false
-    disable_apparmor = false
-    disable_cgroup = false
-    disable_hugetlb_controller = true
-    disable_proc_mount = false
-    disable_tcp_service = true
-    drain_exec_sync_io_timeout = "0s"
-    enable_cdi = false
-    enable_selinux = false
-    enable_tls_streaming = false
-    enable_unprivileged_icmp = false
-    enable_unprivileged_ports = false
-    ignore_deprecation_warnings = []
-    ignore_image_defined_volumes = false
-    image_pull_progress_timeout = "5m0s"
-    image_pull_with_sync_fs = false
-    max_concurrent_downloads = 3
-    max_container_log_line_size = 16384
-    netns_mounts_under_state_dir = false
-    restrict_oom_score_adj = false
-    sandbox_image = "registry.k8s.io/pause:3.8"
-    selinux_category_range = 1024
-    stats_collect_period = 10
-    stream_idle_timeout = "4h0m0s"
-    stream_server_address = "127.0.0.1"
-    stream_server_port = "0"
-    systemd_cgroup = false
-    tolerate_missing_hugetlb_controller = true
-    unset_seccomp_profile = ""
-
-    [plugins."io.containerd.grpc.v1.cri".cni]
-      bin_dir = "/opt/cni/bin"
-      conf_dir = "/etc/cni/net.d"
-      conf_template = ""
-      ip_pref = ""
-      max_conf_num = 1
-      setup_serially = false
-
-    [plugins."io.containerd.grpc.v1.cri".containerd]
-      default_runtime_name = "runc"
-      disable_snapshot_annotations = true
-      discard_unpacked_layers = false
-      ignore_blockio_not_enabled_errors = false
-      ignore_rdt_not_enabled_errors = false
-      no_pivot = false
-      snapshotter = "overlayfs"
-
-      [plugins."io.containerd.grpc.v1.cri".containerd.default_runtime]
-        base_runtime_spec = ""
-        cni_conf_dir = ""
-        cni_max_conf_num = 0
-        container_annotations = []
-        pod_annotations = []
-        privileged_without_host_devices = false
-        privileged_without_host_devices_all_devices_allowed = false
-        runtime_engine = ""
-        runtime_path = ""
-        runtime_root = ""
-        runtime_type = ""
-        sandbox_mode = ""
-        snapshotter = ""
-
-        [plugins."io.containerd.grpc.v1.cri".containerd.default_runtime.options]
-
-      [plugins."io.containerd.grpc.v1.cri".containerd.runtimes]
-
-        [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc]
-          base_runtime_spec = ""
-          cni_conf_dir = ""
-          cni_max_conf_num = 0
-          container_annotations = []
-          pod_annotations = []
-          privileged_without_host_devices = false
-          privileged_without_host_devices_all_devices_allowed = false
-          runtime_engine = ""
-          runtime_path = ""
-          runtime_root = ""
-          runtime_type = "io.containerd.runc.v2"
-          sandbox_mode = "podsandbox"
-          snapshotter = ""
-
-          [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
-            BinaryName = ""
-            CriuImagePath = ""
-            CriuPath = ""
-            CriuWorkPath = ""
-            IoGid = 0
-            IoUid = 0
-            NoNewKeyring = false
-            NoPivotRoot = false
-            Root = ""
-            ShimCgroup = ""
-            SystemdCgroup = true
-
-      [plugins."io.containerd.grpc.v1.cri".containerd.untrusted_workload_runtime]
-        base_runtime_spec = ""
-        cni_conf_dir = ""
-        cni_max_conf_num = 0
-        container_annotations = []
-        pod_annotations = []
-        privileged_without_host_devices = false
-        privileged_without_host_devices_all_devices_allowed = false
-        runtime_engine = ""
-        runtime_path = ""
-        runtime_root = ""
-        runtime_type = ""
-        sandbox_mode = ""
-        snapshotter = ""
-
-        [plugins."io.containerd.grpc.v1.cri".containerd.untrusted_workload_runtime.options]
-
-    [plugins."io.containerd.grpc.v1.cri".image_decryption]
-      key_model = "node"
-
-    [plugins."io.containerd.grpc.v1.cri".registry]
-      config_path = ""
-
-      [plugins."io.containerd.grpc.v1.cri".registry.auths]
-
-      [plugins."io.containerd.grpc.v1.cri".registry.configs]
-
-      [plugins."io.containerd.grpc.v1.cri".registry.headers]
-
-      [plugins."io.containerd.grpc.v1.cri".registry.mirrors]
-
-    [plugins."io.containerd.grpc.v1.cri".x509_key_pair_streaming]
-      tls_cert_file = ""
-      tls_key_file = ""
-
-  [plugins."io.containerd.internal.v1.opt"]
-    path = "/opt/containerd"
-
-  [plugins."io.containerd.internal.v1.restart"]
-    interval = "10s"
-
-  [plugins."io.containerd.internal.v1.tracing"]
-    sampling_ratio = 1.0
-    service_name = "containerd"
-
-  [plugins."io.containerd.metadata.v1.bolt"]
-    content_sharing_policy = "shared"
-
-  [plugins."io.containerd.monitor.v1.cgroups"]
-    no_prometheus = false
-
-  [plugins."io.containerd.nri.v1.nri"]
-    disable = true
-    disable_connections = false
-    plugin_config_path = "/etc/nri/conf.d"
-    plugin_path = "/opt/nri/plugins"
-    plugin_registration_timeout = "5s"
-    plugin_request_timeout = "2s"
-    socket_path = "/var/run/nri/nri.sock"
-
-  [plugins."io.containerd.runtime.v1.linux"]
-    no_shim = false
-    runtime = "runc"
-    runtime_root = ""
-    shim = "containerd-shim"
-    shim_debug = false
-
-  [plugins."io.containerd.runtime.v2.task"]
-    platforms = ["linux/amd64"]
-    sched_core = false
-
-  [plugins."io.containerd.service.v1.diff-service"]
-    default = ["walking"]
-
-  [plugins."io.containerd.service.v1.tasks-service"]
-    blockio_config_file = ""
-    rdt_config_file = ""
-
-  [plugins."io.containerd.snapshotter.v1.aufs"]
-    root_path = ""
-
-  [plugins."io.containerd.snapshotter.v1.blockfile"]
-    fs_type = ""
-    mount_options = []
-    root_path = ""
-    scratch_file = ""
-
-  [plugins."io.containerd.snapshotter.v1.btrfs"]
-    root_path = ""
-
-  [plugins."io.containerd.snapshotter.v1.devmapper"]
-    async_remove = false
-    base_image_size = ""
-    discard_blocks = false
-    fs_options = ""
-    fs_type = ""
-    pool_name = ""
-    root_path = ""
-
-  [plugins."io.containerd.snapshotter.v1.native"]
-    root_path = ""
-
-  [plugins."io.containerd.snapshotter.v1.overlayfs"]
-    mount_options = []
-    root_path = ""
-    sync_remove = false
-    upperdir_label = false
-
-  [plugins."io.containerd.snapshotter.v1.zfs"]
-    root_path = ""
-
-  [plugins."io.containerd.tracing.processor.v1.otlp"]
-    endpoint = ""
-    insecure = false
-    protocol = ""
-
-  [plugins."io.containerd.transfer.v1.local"]
-    config_path = ""
-    max_concurrent_downloads = 3
-    max_concurrent_uploaded_layers = 3
-
-    [[plugins."io.containerd.transfer.v1.local".unpack_config]]
-      differ = ""
-      platform = "linux/amd64"
-      snapshotter = "overlayfs"
-
-[proxy_plugins]
-
-[stream_processors]
-
-  [stream_processors."io.containerd.ocicrypt.decoder.v1.tar"]
-    accepts = ["application/vnd.oci.image.layer.v1.tar+encrypted"]
-    args = ["--decryption-keys-path", "/etc/containerd/ocicrypt/keys"]
-    env = ["OCICRYPT_KEYPROVIDER_CONFIG=/etc/containerd/ocicrypt/ocicrypt_keyprovider.conf"]
-    path = "ctd-decoder"
-    returns = "application/vnd.oci.image.layer.v1.tar"
-
-  [stream_processors."io.containerd.ocicrypt.decoder.v1.tar.gzip"]
-    accepts = ["application/vnd.oci.image.layer.v1.tar+gzip+encrypted"]
-    args = ["--decryption-keys-path", "/etc/containerd/ocicrypt/keys"]
-    env = ["OCICRYPT_KEYPROVIDER_CONFIG=/etc/containerd/ocicrypt/ocicrypt_keyprovider.conf"]
-    path = "ctd-decoder"
-    returns = "application/vnd.oci.image.layer.v1.tar+gzip"
-
-[timeouts]
-  "io.containerd.timeout.bolt.open" = "0s"
-  "io.containerd.timeout.metrics.shimstats" = "2s"
-  "io.containerd.timeout.shim.cleanup" = "5s"
-  "io.containerd.timeout.shim.load" = "5s"
-  "io.containerd.timeout.shim.shutdown" = "3s"
-  "io.containerd.timeout.task.state" = "2s"
-
-[ttrpc]
-  address = ""
-  gid = 0
-  uid = 0
-
-EOF
+containerd config default > /etc/containerd/config.toml
+sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
+sed -i 's/level = ""/level = "debug"/' /etc/containerd/config.toml
+sed -i 's/snapshotter = "overlayfs"/snapshotter = "native"/' /etc/containerd/config.toml
 
 ## Preflight checks
 # See: https://github.com/go4clouds/cloud-infra/blob/main/libvirt/provision-os-node.sh
